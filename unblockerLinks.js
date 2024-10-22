9f3ef2e48d6fd3c0df9b0410e3b0db852378b819
WhitehawkGp <gavin75548f@gmail.com>
2024-10-22 16:32:28 -0400

    Add files via upload

---
+++ unblockerLinks.js
@@ -0,0 +1,4 @@
+// Select the paragraph and change its font size and margin-top
+const text = document.getElementById("myText");
+text.style.fontSize = "16px";  // Set font size
+text.style.marginTop = "50px"; // Move the text down
